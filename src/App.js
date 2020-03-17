04217661ad5fc984b3e9a990f03c944c3e4e8e2f
Nitin Gupta <nitingupta@sprinkles-MacBook-Air.local>
2020-03-17 13:01:19 +0530

    Deployment Commit #2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello World<br></br>
           Nitin this side
         </p>
       </header>
